4836df46576e1778c5216ca8f5d5c8b22e23e9d0
Abbey Yacoe <ayacoe@lsst.org>
2018-10-10 13:55:19 -0700

    Test to see if BrowserRouter causing bad deployment

--- src/index.js
+++ src/index.js
@@ -6,15 +6,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-render(
-  <BrowserRouter>
-    <div>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/coloring-the-universe" component={App} />
-        <Route path="/mapping-the-milky-way" component={MilkyWay} />
-      </Switch>
-    </div>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
