432871e3590fc994fd50bff65460a99d62e6e3f3
Abbey Yacoe <ayacoe@lsst.org>
2018-10-11 15:16:36 -0700

    revert for netlify testing

--- src/index.js
+++ src/index.js
@@ -6,19 +6,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-render(
-  <BrowserRouter>
-    <Switch>
-      <Route path={process.env.PUBLIC_URL + '/'} component={App} />
-      <Route
-        path={process.env.PUBLIC_URL + '/coloring-the-universe'}
-        component={App}
-      />
-      <Route
-        path={process.env.PUBLIC_URL + '/mapping-the-milky-way'}
-        component={MilkyWay}
-      />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
